619e879621d8d51b724a980c6ccd35874a514a39
Seth Weiner <sweiner@fonteva.com>
2020-09-14 15:22:35 -0400

    build

--- server.js
+++ server.js
@@ -28,26 +28,26 @@ app.get("/chat", function (rer, res) {
   res.sendFile(__dirname + "/views/chat-now.html");
 });
 
-io.sockets.on("connection", function (socket) {
+io.sockets.on('connection', function (socket) {
   connections.push(socket);
-  console.log("Connected: %s sockets connected", connections.length);
+  console.log('Connected: %s sockets connected', connections.length);
 
-  socket.on("disconnect", function (data) {
+  socket.on('disconnect', function (data) {
     users.splice(users.indexOf(socket.username), 1);
     updateUsernames();
-    connections.splice(connections.indexOf(socket), 1);
-    io.emit("disconnected", socket.username);
-    console.log("Disconnected: %s sockets connected", connections.length);
+    connections.splice(connections.indexOf(socket), 1)
+    io.emit('disconnected', socket.username);
+    console.log('Disconnected: %s sockets connected', connections.length);
   });
 
-  socket.on("send message", function (data) {
-    io.sockets.emit("new message", {
+  socket.on('send message', function (data) {
+    io.sockets.emit('new message', {
       msg: data,
-      user: socket.username,
+      user: socket.username
     });
   });
 
-  socket.on("add user", function (data, callback) {
+  socket.on('add user', function (data, callback) {
     socket.username = data;
 
     if (users.indexOf(socket.username) > -1) {
@@ -58,50 +58,35 @@ io.sockets.on("connection", function (socket) {
       callback(true);
 
       if (Object.keys(users).length == 2) {
-        io.emit("connected", socket.username);
-        io.emit("game start");
+        io.emit('connected', socket.username);
+        io.emit('game start');
       }
     }
   });
 
-  socket.on("player choice", function (username, choice) {
+  socket.on('player choice', function (username, choice) {
     choices.push({
-      user: username,
-      choice: choice,
+      'user': username,
+      'choice': choice
     });
-    console.log("%s chose %s.", username, choice);
+    console.log('%s chose %s.', username, choice);
 
     if (choices.length == 2) {
-      console.log("[socket.io] Both players have made choices.");
+      console.log('[socket.io] Both players have made choices.');
 
-      switch (choices[0]["choice"]) {
-        case "rock":
-          switch (choices[1]["choice"]) {
-            case "rock":
-              io.emit("tie", choices);
-              results.push({
-                round: currentRound,
-                winner: 0,
-                choices: choices,
-              });
+      switch (choices[0]['choice']) {
+        case 'rock':
+          switch (choices[1]['choice']) {
+            case 'rock':
+              io.emit('tie', choices);
               break;
 
-            case "paper":
-              io.emit("player 2 win", choices);
-              results.push({
-                round: currentRound,
-                winner: 2,
-                choices: choices,
-              });
+            case 'paper':
+              io.emit('player 2 win', choices);
               break;
 
-            case "scissors":
-              io.emit("player 1 win", choices);
-              results.push({
-                round: currentRound,
-                winner: 1,
-                choices: choices,
-              });
+            case 'scissors':
+              io.emit('player 1 win', choices);
               break;
 
             default:
@@ -109,33 +94,18 @@ io.sockets.on("connection", function (socket) {
           }
           break;
 
-        case "paper":
-          switch (choices[1]["choice"]) {
-            case "rock":
-              io.emit("player 1 win", choices);
-              results.push({
-                round: currentRound,
-                winner: 1,
-                choices: choices,
-              });
+        case 'paper':
+          switch (choices[1]['choice']) {
+            case 'rock':
+              io.emit('player 1 win', choices);
               break;
 
-            case "paper":
-              io.emit("tie", choices);
-              results.push({
-                round: currentRound,
-                winner: 0,
-                choices: choices,
-              });
+            case 'paper':
+              io.emit('tie', choices);
               break;
 
-            case "scissors":
-              io.emit("player 2 win", choices);
-              results.push({
-                round: currentRound,
-                winner: 2,
-                choices: choices,
-              });
+            case 'scissors':
+              io.emit('player 2 win', choices);
               break;
 
             default:
@@ -143,34 +113,18 @@ io.sockets.on("connection", function (socket) {
           }
           break;
 
-        case "scissors":
-          switch (choices[1]["choice"]) {
-            case "rock":
-              io.emit("player 2 win", choices);
-              results.push({
-                round: currentRound,
-                winner: 2,
-                choices: choices,
-              });
+        case 'scissors':
+          switch (choices[1]['choice']) {
+            case 'rock':
+              io.emit('player 2 win', choices);
               break;
 
-            case "paper":
-              io.emit("player 1 win", choices);
-              results.push({
-                round: currentRound,
-                winner: 1,
-                choices: choices,
-              });
+            case 'paper':
+              io.emit('player 1 win', choices);
               break;
 
-            case "scissors":
-              io.emit("tie", choices);
-
-              results.push({
-                round: currentRound,
-                winner: 0,
-                choices: choices,
-              });
+            case 'scissors':
+              io.emit('tie', choices);
               break;
 
             default:
@@ -181,13 +135,12 @@ io.sockets.on("connection", function (socket) {
         default:
           break;
       }
-      io.emit("end round", results);
-      currentRound++;
+
       choices = [];
     }
   });
 
   function updateUsernames() {
-    io.sockets.emit("get user", users);
+    io.sockets.emit('get user', users);
   }
 });
